70ea0b299117d5442dc9b6208de75c613a3829aa
Lorena <pbelarmino18@gmail.com>
2024-07-31 19:45:41 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,158 @@
+//Exercícios do freecode camp
+//Ex 1:
+console.log("Let's build a pyramid using JavaScript basics:");
+console.log("    #    ");
+console.log("   ###   ");
+console.log("  #####  ");
+console.log(" ####### ");
+console.log("#########");
+
+//Ex 2:
+let character;
+
+//Ex 3:
+let character = "Hello";
+
+//Ex 4:
+let character = 'Hello';
+
+//Ex 5:
+let character = 'Hello';
+console.log(character);
+
+//Ex 6:
+let character = 'Hello';
+console.log(character);
+character = "World";
+
+//Ex 7:
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character)
+
+//Ex 8:
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
+let secondCharacter;
+
+//Ex 9:
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+console.log(secondCharacter);
+
+//Ex 10:
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = "Test";
+console.log(secondCharacter);
+
+//Ex 11:
+secondCharacter = "Test";
+secondCharacter = character;
+
+//Ex 12:
+let character = 'Hello';
+
+//Ex 13:
+let profession;
+let age;
+profession = "teacher";
+console.log(profession);
+console.log(age);
+
+//Ex 14:
+//o que sobrou foi:
+let character = "Hello";
+
+//Ex 15:
+let character = 'Hello';
+let count;
+count = 8;
+
+//Ex 16:
+let character = 'Hello';
+let count = 8;
+console.log(count + 1)
+
+
+//Ex 17:
+let count = 8;
+
+//Ex 18:
+let character = 'Hello';
+let count = 8;
+let rows = [];
+
+//Ex 19:
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+//Ex 20:
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+
+//Ex 21:
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows[2] = 10;
+console.log(rows)
+
+//Ex 22:
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows[0]);
+rows[rows.length - 1] = 10; //obtendo o último item do array com length - 1.
+
+//Ex 23:
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+console.log(rows);
+
+//Ex 24:
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities)
+cities[2] = "Mexico City";
+console.log(cities)
+
+//Ex 25:
+//A resposta é remover o código anterior.
+
+//Ex 26:
+//obs: Use .push() to add the string "freeCodeCamp" to the end of your rows array. Add this code before your console.log so you can see the change you made to your array.
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+console.log(rows);
+
+//Ex 27:
+//.pop() method. It removes the last element from an array and returns that element. When a method returns a value, you can think of it as giving the value back to you, making it available for use in other parts of your code.
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp");
+let popped = rows.pop();
+console.log(popped);
+
+//Ex 28:
+//.push() returns the new length of the array, after adding the value you give it.
+let pushed = rows.push("freeCodeCamp");
+console.log(pushed)
+
+//Ex 29:
+//A resposta é remover o código anterior.
+
+//Ex 30:
+let character = "Hello";
+let count = 8;
+let rows = [];
